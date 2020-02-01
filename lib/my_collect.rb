f692b4e23be6cb2001b8c04ad263bf5360b2d8cd
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 17:06:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
+# .map iteration
 def my_collect(array)
 new_array = []
-i = 0
+ i = 0
   while i < array.length
-      #new_array << yield array[i]
-      new_array.push(yield array[i])
+      new_array << yield array[i]
+      #new_array.push(yield array[i])
       i += 1
   end
-  new array 
 end
 
